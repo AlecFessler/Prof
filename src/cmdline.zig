f569b8d182715ed9aeb9bdbbc0dcf9d187609ed2
Alec Fessler <afessler1998@gmail.com>
2026-08-12 18:35:29 -0500

    initial process fork -> SIGSTOP -> execve on SIGCONT flow established

--- src/cmdline.zig
+++ src/cmdline.zig
@@ -1,17 +1,17 @@
 const std = @import("std");
 
 pub const Config = struct {
-    target: [:0]const u8,
+    target_path: [:0]const u8,
 };
 
 pub fn parseCmdline(args: []const [:0]const u8) !Config {
-    var target_exe: [:0]const u8 = undefined;
+    var target_path: [:0]const u8 = undefined;
     for (args, 0..) |arg, arg_count| {
         // the first arg parsed is the name of the current running executable
         if (arg_count == 0) continue;
 
         // the second arg is expected to be path of the target binary
-        if (arg_count == 1) target_exe = arg;
+        if (arg_count == 1) target_path = arg;
     }
 
     if (args.len <= 1) {
@@ -19,6 +19,6 @@ pub fn parseCmdline(args: []const [:0]const u8) !Config {
     }
 
     return .{
-        .target = target_exe,
+        .target_path = target_path,
     };
 }
--- src/main.zig
+++ src/main.zig
@@ -9,6 +9,8 @@ const checkLinuxFd = linux_error.checkFd;
 
 const parseCmdline = prof.cmdline.parseCmdline;
 
+const LOOP_UPPER_BOUND = 50000;
+
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
     const arena: std.mem.Allocator = init.arena.allocator();
@@ -26,5 +28,56 @@ pub fn main(init: std.process.Init) !void {
         },
     };
 
-    try stdout.print("Target: {s}\n", .{config.target});
+    const target_pid = try forkAndWait(config.target_path);
+
+    try stdout.print("Target pid: {}\n", .{target_pid});
+}
+
+/// Fork the profiler process and on success, the forked process
+/// SIGSTOPs itself. The caller waits until the fork stops, and then
+/// the fork's pid is returned. When the profiler SIGCONTs the fork,
+/// it will execve the target path. This provides the profiler with a
+/// window to set up monitoring before the target begins execution.
+fn forkAndWait(target_path: [:0]const u8) !std.os.linux.pid_t {
+    var target_pid: std.os.linux.pid_t = undefined;
+    {
+        const rc = std.os.linux.fork();
+        try checkLinuxError(rc);
+        target_pid = @intCast(rc);
+
+        if (rc == 0) {
+            // the kernel returns 0 on fork() to the spawned process,
+            // thus it enters this branch, but it must retreive it's own pid
+
+            const pid = std.os.linux.getpid();
+            try checkLinuxError(std.os.linux.kill(pid, std.os.linux.SIG.STOP));
+
+            const argv = [_:null]?[*:0]const u8{target_path.ptr};
+            const env = [_:null]?[*:0]const u8{};
+
+            // execve only returns if there was an error
+            try checkLinuxError(std.os.linux.execve(target_path, &argv, &env));
+        }
+    }
+
+    // fork() succeeded, wait for the forked process to SIGSTOP
+    {
+        var status: u32 = 0;
+        var iterations: u32 = 0;
+        while (iterations < LOOP_UPPER_BOUND) : (iterations += 1) {
+            const rc = std.os.linux.waitpid(
+                target_pid,
+                &status,
+                std.os.linux.W.UNTRACED,
+            );
+            if (std.os.linux.errno(rc) == .INTR) continue;
+            try checkLinuxError(rc);
+            break;
+        }
+        if (!std.os.linux.W.IFSTOPPED(status)) {
+            return error.ForkFailedToStop;
+        }
+    }
+
+    return target_pid;
 }
